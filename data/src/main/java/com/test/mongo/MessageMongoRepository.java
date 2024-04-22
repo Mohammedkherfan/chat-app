746f4ea85cb020c168490f71191455270ce6f40f
MKherfan <mohammed.kherfan@maf.ae>
2024-04-22 16:39:07 +0400

    add message model

--- data/src/main/java/com/test/mongo/MessageMongoRepository.java
+++ data/src/main/java/com/test/mongo/MessageMongoRepository.java
@@ -1,10 +1,14 @@
 package com.test.mongo;
 
+import com.test.bo.MessageBo;
 import com.test.entity.MessageEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageMongoRepository extends MongoRepository<MessageEntity, Long> {
 
+    List<MessageEntity> findAllByChatId(String chatId);
 }
--- data/src/main/java/com/test/repository/impl/MessageRepositoryImpl.java
+++ data/src/main/java/com/test/repository/impl/MessageRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.test.repository.impl;
 
+import com.test.bo.MessageBo;
 import com.test.map.MessageMap;
 import com.test.mongo.MessageMongoRepository;
 import com.test.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class MessageRepositoryImpl implements MessageRepository {
@@ -13,4 +16,14 @@ public class MessageRepositoryImpl implements MessageRepository {
     private MessageMongoRepository repository;
     @Autowired
     private MessageMap map;
+
+    @Override
+    public MessageBo save(MessageBo messageBo) {
+        return map.toBo(repository.save(map.toEntity(messageBo)));
+    }
+
+    @Override
+    public List<MessageBo> findAllByChatId(String chatId) {
+        return repository.findAllByChatId(chatId).stream().map(entity -> map.toBo(entity)).toList();
+    }
 }
